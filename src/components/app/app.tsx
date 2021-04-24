d1b3f5cb4bd9b2cbee14ddd748939d59b6f5ced5
prnmxm <prnmxm@gmail.com>
2021-04-24 23:04:49 +0300

    header

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,13 @@
 import { AppHeader } from '../appHeader'
+import { BurgerConstructor } from '../burgerConstructor'
+import { BurgerIngredients } from '../burgerIngredients'
 
 function App () {
     return (
         <>
             <AppHeader /> 
+            <BurgerConstructor /> 
+            <BurgerIngredients /> 
         </>
     )
 }
--- src/components/appHeader/appHeader.tsx
+++ src/components/appHeader/appHeader.tsx
@@ -1,7 +1,24 @@
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import style from './appHeader.module.scss';
+
 function AppHeader () {
     return (
-        <header>
-
+        <header className={style.header}>
+            <div className={style.container}>
+                <ul className={style.menu}>
+                    <li className={style.item}>
+                        <a href="#constructor" className={`text text_type_main-default ${style.button} ${style.active}`}>
+                        <BurgerIcon type="primary" /> Конструктор</a>
+                        </li>
+                    <li className={style.item}>
+                        <a href="#lenta" className={`text text_type_main-default ${style.button}`}>
+                        <ListIcon type="secondary" /> Лента заказов</a>
+                        </li>
+                </ul>
+                <Logo />
+                <a href="#cabinet" className={`${style.button}`}>
+                <ProfileIcon type="secondary" /> Личный кабинет</a>
+            </div>
         </header>
     )
 }
